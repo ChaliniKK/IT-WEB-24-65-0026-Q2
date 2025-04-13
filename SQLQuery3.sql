7f409ddb3473d08a0e6d2c6175a4d6fb141d824f
ChaliniKK <chalinikaushalya14@gmail.com>
2025-04-13 16:23:11 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,14 @@
+-- sample data
+INSERT INTO Courses (Name, LecturerName) VALUES 
+('Web Development', 'M.M. Herath'),
+('Graphic Design', 'J.S.V. Piyasena'),
+('Mobile App Development', 'K.K.S. Dias'),
+('Java', 'U.H.S. Perera');
+
+INSERT INTO Students (Name, City, CourseId) VALUES 
+('Kasun Gamage', 'Kandy', 2),
+('Daniel Sam', 'Jaffna', 3),
+('Hansi Silva', 'Colombo', 1),
+('Raniou Heath', 'Matara', 3),
+('Praneeth Wijesinghe', 'Kandy', 4),
+('Nuwani Herath', 'Rathnapura', 1);
